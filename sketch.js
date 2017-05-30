f207d7675d9cc4fd41d1523173a5f38bf62633bf
Tyler Barton <tylerjamesbar@gmail.com>
2017-05-30 11:21:52 -0400

    canvas thing

--- sketch.js
+++ sketch.js
@@ -49,6 +49,9 @@ function draw(){
   document.getElementById("yPos").innerHTML = s.y;
   document.getElementById("xSpd").innerHTML = speed*10;
   document.getElementById("scl").innerHTML = scl;
+  document.getElementById("canvasWidthBox").value;
+  document.getElementById("canvasHeightBox").value;
+
 
 } //draw
 
@@ -180,7 +183,7 @@ function chngTail(input){
   s.maxTailLength = s.maxTailLength + input;
 }//END of chngTail
 
-//reset canvas and snake properties 
+//reset canvas and snake properties
 function Reset(){
   s;
   scl = 10;
@@ -203,6 +206,7 @@ function Reset(){
   s.colorDirection = true;
   s.currTailLength = 0; //length in pixels
   s.maxTailLength = 1000;
+  StopStart(1);
 }
 
 function DEBUG(input){
